a48f33831770b4fb8a6dd4c2a2ecac27a19e01a7
Maze Winther <innoworldgames@gmail.com>
2024-10-03 01:40:57 +0200

    Hide sidebar on small screens

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -25,7 +25,7 @@ export default function Sidebar() {
   };
 
   return (
-    <aside className="w-64 h-[calc(100vh-64px)] bg-background border-r border-border sticky top-16 flex-shrink-0">
+    <aside className="w-64 h-[calc(100vh-64px)] bg-background border-r border-border sticky top-16 flex-shrink-0 hidden lg:block">
       <nav className="p-4">
         <ul className="space-y-2">
           {links.map((link) => (
