7556b815b97405c7742b3fc39d7655ecd41f0294
Maze Winther <innoworldgames@gmail.com>
2024-10-03 15:54:20 +0200

    Reduce space between "Skills" title and logos

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -7,7 +7,7 @@ export default function Skills() {
       className="flex flex-col gap-8 items-center py-24 w-full"
       id="skills-section"
     >
-      <div className="flex flex-col items-center gap-0 md:gap-32 w-full">
+      <div className="flex flex-col items-center gap-0 md:gap-20 w-full">
         <h2 className="text-4xl font-bold text-primary space-y-4 w-full text-center">
           Skills
         </h2>
